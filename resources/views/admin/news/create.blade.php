c25e7b03cf1e3dae58e847560471ce4d27ebc769
ForsanNemah <forsan20172017@gmail.com>
2023-02-19 12:09:45 +0300

    a

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -127,7 +127,7 @@
 
 
                           <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">File</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-end">Language</label>
 
                             <div class="col-md-6">
                    
